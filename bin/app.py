dfd23b5fef586fdac6964024863a02eaf1768419
torjeikenes <torje.n.eikenes@gmail.com>
2016-09-30 17:48:04 +0200

    initial commit

---
+++ bin/app.py
@@ -0,0 +1,23 @@
+import web
+from MarkovRap import testMarkov
+
+urls = (
+    '/hello', 'index'
+)
+
+ 
+app = web.application(urls, globals())
+
+render = web.template.render('templates/', base="layout")
+
+class index:
+    def GET(self):
+        return render.hello_form()
+
+    def POST(self):
+        form = web.input(startword="")
+        lyric = testMarkov(form.startword)
+        return render.index(lyric = lyric)
+
+if __name__ == "__main__":
+    app.run()
